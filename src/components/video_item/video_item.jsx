4aa954f2c67436cebe70f379febe3c4d8e6fbca1
jisooyoon <ygs12567@gmail.com>
2022-03-27 20:08:33 +0900

    Add videoList
    - video와 videoList title,description 추가
    - style 수정

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => {
-  return <h1>{props.video.title}</h1>;
+/*props에서도 deconstructing이 가능하다 :props중에 특정한 요소를 가져오고싶다면,
+  구조분해할당을 통해서 인자를 전달하게되면 된다. 그리고 video안에있는 snippet을 가져오려하면 
+  아래처럼 deconstructing을 해주면된다
+ */
+const VideoItem = ({ video: { snippet } }) => {
+  return (
+    <li className={styles.videoCard}>
+      <figure className={styles.thumbnailImg}>
+        <img src={snippet.thumbnails.medium.url} alt="썸네일 이미지" />
+      </figure>
+      <div className={styles.descriptions}>
+        <p className={styles.videoTitle}>{snippet.title}</p>
+        <p className={styles.channelName}>{snippet.channelTitle}</p>
+      </div>
+    </li>
+  );
 };
 
 export default VideoItem;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
-
+import styles from './video_list.module.css';
 const VideoList = (props) => {
   return (
-    <ul>
+    <ul className={styles.videoList}>
       {props.videos &&
         props.videos.map((video) => {
-          return <VideoItem key={video.id} video={video.snippet} />;
+          return <VideoItem key={video.id} video={video} />;
         })}
     </ul>
   );
